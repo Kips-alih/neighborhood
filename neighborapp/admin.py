d2420d4947c8ae7e9a752d92f9710df29b45c1c5
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-22 06:31:59 +0300

    Create and register profile and location models

--- neighborapp/admin.py
+++ neighborapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from neighborapp.models import Profile,Location
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Location)
--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField(max_length=300,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location=models.CharField(max_length=50,null=True)
+
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    
+
